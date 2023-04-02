05271a61fc3cd6decbef8e28d9f8b1df160e820d
Vinay Namala <vinayn@mcci.com>
2023-04-02 20:35:02 +0530

    fix #1 string Rev

---
+++ src/StrRev.c
@@ -0,0 +1,30 @@
+#include <string.h> // for strlen()
+#include <stdio.h>
+
+// reverse the given null-terminated string in place
+void inplace_reverse(char * str)
+{
+  if (str)
+  {
+    char * end = str + strlen(str) - 1;
+
+    // swap the values in the two given variables
+    // XXX: fails when a and b refer to same memory location
+#   define XOR_SWAP(a,b) do\
+    {\
+      a ^= b;\
+      b ^= a;\
+      a ^= b;\
+    } while (0)
+
+    // walk inwards from both ends of the string, 
+    // swapping until we get to the middle
+    while (str < end)
+    {
+      XOR_SWAP(*str, *end);
+      str++;
+      end--;
+    }
+#   undef XOR_SWAP
+  }
+}
